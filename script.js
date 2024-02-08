18aff107d24db33ccca6712a4e15137203897eeb
leandroricke100 <leandro_henrique_100@hotmail.com>
2024-02-08 09:30:37 -0300

    update

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function nextImage() {
   }
   document.getElementById('radio' + count).checked = true
 }
+
+
+const slides = document.querySelectorAll('.slide-depoimentos');
+const slider = document.querySelector('.slides-depoimentos');
+
+let index = 0;
+
+function nextSlide() {
+  index++;
+  if (index === slides.length) {
+    index = 0;
+  }
+  updateSlide();
+}
+
+function updateSlide() {
+  slider.style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(nextSlide, 3000);
+
