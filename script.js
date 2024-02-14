b16b1207050dd6b13fd30472351fdd672f2c0165
leandroricke100 <leandro_henrique_100@hotmail.com>
2024-02-14 18:10:26 -0300

    criancao do menu mobile

--- script.js
+++ script.js
@@ -77,14 +77,21 @@ const handleMediaQueryChange = (mediaQuery) => {
   if (mediaQuery.matches) {
     $('#cliente-mobile').show();
     $('.slider-clientes').hide();
+    $('#menu-mobile').show();
   } else {
 
     $('#cliente-mobile').hide();
     $('.slider-clientes').show();
+    $('#menu-mobile').hide();
 
   }
 };
 handleMediaQueryChange(mediaQuery);
 mediaQuery.addListener(handleMediaQueryChange);
+
+
+function menuMobile() {
+  alert('teste');
+}
 
 
